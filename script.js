89be6f97196ea1600ae420e336b0069c24cf3a78
Dfnkk <60377182+Dfnkk@users.noreply.github.com>
2020-05-13 17:27:22 -0700

    done!

---
+++ script.js
@@ -0,0 +1,19 @@
+let tik = document.getElementById('tik');
+let rock = document.getElementById('rock');
+
+
+rock.onmouseout = function() {
+    rock.style.backgroundColor = 'white';
+}
+
+rock.onmouseover = function() {
+    rock.style.backgroundColor = 'red';
+}
+
+tik.onmouseout = function() {
+    tik.style.backgroundColor = 'white';
+}
+
+tik.onmouseover = function() {
+    tik.style.backgroundColor = 'red';
+}
